a4095bef2d2de19cad1f891ad05ab959120a40d4
Garland <garland220@yahoo.com>
2017-11-28 17:15:22 -0500

    Improved intersection

--- server/Weather.ts
+++ server/Weather.ts
@@ -6,8 +6,35 @@ export class Weather {
   private bounds:Rectangle2D;
   private map:Map;
 
+  private temperature:number = 0;
+
+  public get Bounds():Rectangle2D {
+    return this.bounds;
+  }
+
   constructor(map:Map) {
-      this.map = map;
+    this.map = map;
+  }
+
+  public CheckIntersection(rectangle1:Rectangle2D, rectangle2:Rectangle2D):boolean {
+    if (rectangle1.Start.X >= (rectangle2.Start.X + rectangle2.Width)) {
+      return false;
+    }
+    if (rectangle2.Start.X >= (rectangle1.Start.X + rectangle1.Width)) {
+      return false;
+    }
+    if (rectangle1.Start.Y >= (rectangle2.Start.Y + rectangle2.Height)) {
+      return false;
+    }
+    if (rectangle2.Start.Y >= (rectangle1.Start.Y + rectangle1.Height)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public IntersectsWith(weather:Weather):boolean {
+    return this.CheckIntersection(this.Bounds, weather.Bounds);
   }
 
   public ReCenter(point2D:Point2D):void {
